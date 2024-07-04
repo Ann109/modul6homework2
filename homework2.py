103417dcf7094bdbb646800233bce058ace81ad9
Ann109 <andersonhloa@gmail.com>
2024-07-04 16:06:08 +0300

    Update homework2.py

--- homework2.py
+++ homework2.py
@@ -1,32 +1,35 @@
 class Vehicle:
-        __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+    __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
 
-        def __init__(self, owner, _model, _engine_power, _color):
-            self.owner = owner
-            self.__model = _model
-            self.__engine_power = _engine_power
-            self.__color = _color
+    def __init__(self, owner, _model, _engine_power, _color):
+        self.owner = owner
+        self.__model = _model
+        self.__engine_power = _engine_power
+        self.__color = _color
 
+    def get_model(self):
+        return f'модель: {self.__model}'
 
-        def get_model(self):
-            print(f'модель: {self.__model}')
-        def get_horsepower(self):
-            print(f'мощность двигателя: {self.__engine_power}')
-        def get_color(self):
-            print(f'цвет: {self.__color}')
-        def print_info(self):
-            Vehicle.get_model(self)
-            Vehicle.get_horsepower(self)
-            Vehicle.get_color(self)
-            print(f'владелец: {self.owner}')
-        def set_color(self, new_color: str):
-            if new_color.lower() in self.__COLOR_VARIANTS:
-                self.__color = new_color
-            else:
-                print(f'Невозможно покрасить в {new_color}')
+    def get_horsepower(self):
+        return f'мощность двигателя: {self.__engine_power}'
+
+    def get_color(self):
+        return f"цвет: {self.__color}"
+
+    def print_info(self):
+        print(self.get_model()),
+        print(self.get_horsepower()),
+        print(self.get_color()),
+        print(f"владелец: .{self.owner}")
+
+    def set_color(self, new_color: str):
+        if new_color.lower() in self.__COLOR_VARIANTS:
+            self.__color = new_color
+        else:
+            print(f'Невозможно покрасить в {new_color}')
 
 class Sedan(Vehicle):
-        __PASSENGERS_LIMIT = 5
+    __PASSENGERS_LIMIT = 5
 
 
 # Текущие цвета __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
@@ -42,5 +45,6 @@ vehicle1.owner = 'Vasyok'
 
 # Проверяем что поменялось
 vehicle1.print_info()
+
 
 
